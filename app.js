b8109a630c976e2bb0a6bf05e32e3245ed0b9a63
Romina Racca <romina.racca@gmail.com>
2016-05-04 11:14:52 -0300

    configuracion para visualizar la documentacion y definicion de path para los recursos

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict';
 
+var fs      = require('fs-extra');
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 module.exports = app; // for testing
+
+var configCon = JSON.parse(fs.readFileSync('/etc/nodejs-config/mara.json'));
 
 var config = {
   appRoot: __dirname // required config
@@ -11,13 +15,21 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
+  //add swagger-ui
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
+
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
+  // var port = process.env.PORT || 10010;
+  // app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+  // if (swaggerExpress.runner.swagger.paths['/hello']) {
+  //   console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  // }
+
+  console.log(configCon);
+  console.log("Dcoumentacion: http://localhost:8080/docs/#/");
+  var port =  configCon.port;       //process.env.PORT
+  app.listen(port);
 });
